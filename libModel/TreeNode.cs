bdfe0da1b64da1ce7f351eba73189de243bec7eb
GUILLERMO VALENZUELA <kaoscore53@yahoo.com>
2019-10-05 10:02:09 -0500

    Master

---
+++ libModel/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libModel
+{
+    public class TreeNode
+    {
+        public int data;
+        public TreeNode left, right;
+
+        public TreeNode(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+}
